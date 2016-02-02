904935762bd5ebadcfbaddb31880829e30a54154
Travis <travis.daudelin@gmail.com>
2016-02-02 07:49:34 -0800

    Adding personal config; turning on clojure and helm modules

---
+++ personal/personal-config.el
@@ -0,0 +1,12 @@
+;;; Personal Config -- Personal configuration within Emacs Prelude
+
+;;; Commentary:
+
+;;; Code:
+
+;; Start emacs in fullscreen
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+(provide 'personal-config)
+;;; personal-config.el ends here
