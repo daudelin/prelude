43d3dcb3b7463091294d6cfc3f181fc18e2465a4
Travis Daudelin <travis@apptimize.com>
2016-02-02 09:56:29 -0800

    Enabling CSS and SCSS modules

--- prelude-modules.el
+++ prelude-modules.el
@@ -15,7 +15,7 @@
 (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
-;; (require 'prelude-css)
+(require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
@@ -32,7 +32,7 @@
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
-;; (require 'prelude-scss)
+(require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 ;; (require 'prelude-yaml)
